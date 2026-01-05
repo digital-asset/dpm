08e08a16dbfc776337a771a8e9896bee21ff90e8
sammy-da <sammy.abed@digitalasset.com>
2026-01-05 17:18:37 -0500

    include a file containing all compnents' licenses in the tarballs (#7)
    * include a file containing all compnents' licenses in the tarballs
    
    * feedback

---
+++ pkg/licenseutils/licenseutils.go
@@ -0,0 +1,46 @@
+package licenseutils
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"github.com/samber/lo"
+)
+
+const ComponentLicenseFilename = "LICENSE"
+const TarballLicensesFilename = "LICENSES"
+
+// WriteLicensesFile combines components licenses into single LICENSES file.
+// licenses is map from component name to its LICENSE's file path
+func WriteLicensesFile(licenses map[string]string, outputDir string) error {
+	var b strings.Builder
+
+	// Header
+	fmt.Fprintln(&b, "LICENSES")
+	fmt.Fprintln(&b, "============================================================")
+	fmt.Fprintln(&b, "")
+	fmt.Fprintln(&b, "This product includes multiple software components.")
+	fmt.Fprint(&b, "The license terms for each component are provided below.")
+
+	sortedComponents := lo.Keys(licenses)
+	sort.Strings(sortedComponents)
+
+	for _, comp := range sortedComponents {
+		license, err := os.ReadFile(licenses[comp])
+		if err != nil {
+			return err
+		}
+
+		fmt.Fprintln(&b, "")
+		fmt.Fprintln(&b, "")
+		fmt.Fprintln(&b, "------------------------------------------------------------")
+		fmt.Fprintf(&b, "Component: %s\n\n", comp)
+		b.WriteString(strings.TrimSpace(string(license)))
+	}
+	fmt.Fprintln(&b, "")
+
+	return os.WriteFile(filepath.Join(outputDir, TarballLicensesFilename), []byte(b.String()), 0644)
+}
