e41fb7c21da35f3b98ad785f2ddd5097970335c7
sammy-da <sammy.abed@digitalasset.com>
2026-01-06 17:26:43 -0500

    copy LICENSE file on bootstrap (#34)

---
+++ embed.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2017-2025 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package root
+
+import _ "embed"
+
+//go:embed LICENSE
+var License []byte
