4425793958baebe548e0cf8d89dfdb973f5cce11
Brian Healey <brian.healey@digitalasset.com>
2026-01-30 11:19:52 -0500

    add space to comments

--- docs/src/public/manual-install.sh
+++ docs/src/public/manual-install.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 set -xeuo pipefail
 
-#get latest version number
+# get latest version number
 VERSION="$(curl -sS "https://get.digitalasset.com/install/latest")"
 
 # set your architecture to either amd64 | arm64
@@ -10,7 +10,7 @@ ARCH="$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')"
 # set your OS to either darwin or linux
 OS="$(uname | tr '[:upper:]' '[:lower:]')"
 
-#pull down appropriate tarball for your OS and architecture
+# pull down appropriate tarball for your OS and architecture
 readonly TARBALL="dpm-${VERSION}-${OS}-${ARCH}.tar.gz"
 
 # determine location of tarball to download
