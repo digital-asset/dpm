e466250614e125de767579d85158f402ba3cba71
George O. <george.orellana@digitalasset.com>
2026-04-30 13:29:52 -0400

    Component overlay tests implementation (#175)
    * Initial commit for starting optin
    
    * Implement resolve and help, need to implement multiless
    
    * Remove stale code
    
    * Fix overwriting components in multi issue, handle daml.yaml only case
    
    * Fix accidental commenting

--- cmd/dpm/cmd/component_overlay_test.go
+++ cmd/dpm/cmd/component_overlay_test.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"daml.com/x/assistant/pkg/assistantconfig"
@@ -12,6 +13,8 @@ import (
 	"daml.com/x/assistant/pkg/testutil"
 	"daml.com/x/assistant/pkg/utils"
 	"github.com/Masterminds/semver/v3"
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -22,10 +25,10 @@ type ComponentOverlayTestCase struct {
 	PackageComponents            map[string]string
 	ExpectedHelpCommands         []string
 	ExpectedResolutionComponents map[string]string
+	PackageOnly                  bool
 }
 
 var componentOverlayTestCases = []ComponentOverlayTestCase{
-	// TODO fill out add the remaining test cases
 	{
 		Name:                         "1",
 		WorkingDir:                   MultiPackageWorkingDir,
@@ -33,6 +36,98 @@ var componentOverlayTestCases = []ComponentOverlayTestCase{
 		PackageComponents:            nil,
 		ExpectedHelpCommands:         []string{"foo"},
 		ExpectedResolutionComponents: map[string]string{"foo": "0.0.1"},
+	},
+	{
+		Name:                         "2",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1"},
+		PackageComponents:            nil,
+		ExpectedHelpCommands:         []string{"foo"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.1"},
+	},
+	{
+		Name:                         "3",
+		WorkingDir:                   MultiPackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1"},
+		PackageComponents:            map[string]string{"foo": "0.0.2"},
+		ExpectedHelpCommands:         []string{"foo"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.2"},
+	},
+	{
+		Name:                         "4",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1"},
+		PackageComponents:            map[string]string{"foo": "0.0.2"},
+		ExpectedHelpCommands:         []string{"foo"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.2"},
+	},
+	{
+		Name:                         "5",
+		WorkingDir:                   MultiPackageWorkingDir,
+		MultiPackageComponents:       nil,
+		PackageComponents:            map[string]string{"foo": "0.0.2"},
+		ExpectedHelpCommands:         []string{},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.2"},
+	},
+	{
+		Name:                         "6",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       nil,
+		PackageComponents:            map[string]string{"foo": "0.0.2"},
+		ExpectedHelpCommands:         []string{"foo"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.2"},
+	},
+	{
+		Name:                         "7",
+		WorkingDir:                   MultiPackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1", "meep": "0.0.1"},
+		PackageComponents:            nil,
+		ExpectedHelpCommands:         []string{"foo", "meep"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.1", "meep": "0.0.1"},
+	},
+	{
+		Name:                         "8",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1", "meep": "0.0.1"},
+		PackageComponents:            nil,
+		ExpectedHelpCommands:         []string{"foo", "meep"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.1", "meep": "0.0.1"},
+	},
+	{
+		Name:                         "9",
+		WorkingDir:                   MultiPackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1"},
+		PackageComponents:            map[string]string{"meep": "0.0.2"},
+		ExpectedHelpCommands:         []string{"foo"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.1", "meep": "0.0.2"},
+	},
+	{
+		Name:                         "10",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1"},
+		PackageComponents:            map[string]string{"meep": "0.0.2"},
+		ExpectedHelpCommands:         []string{"foo", "meep"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.1", "meep": "0.0.2"},
+	},
+	{
+		//no multi-pkg structure - in pkg dir
+		Name:                         "11",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       nil,
+		PackageComponents:            map[string]string{"foo": "0.0.2"},
+		ExpectedHelpCommands:         []string{"foo"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.2"},
+		PackageOnly:                  true,
+	},
+	{
+		//no multi-pkg structure - in pkg dir
+		Name:                         "12",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       nil,
+		PackageComponents:            nil,
+		ExpectedHelpCommands:         []string{}, //blank DS or nil
+		ExpectedResolutionComponents: map[string]string{},
+		PackageOnly:                  true,
 	},
 }
 
@@ -58,43 +153,50 @@ func (suite *MainSuite) TestComponentOverlay() {
 
 	setupTestCase := func(tc ComponentOverlayTestCase) (dirs TestCaseDirs) {
 		tmpDir := t.TempDir()
-		dirs.MultiPackageDir = filepath.Join(tmpDir, "multi-package")
-		dirs.DamlPackageDir = filepath.Join(dirs.MultiPackageDir, "daml-package")
-		require.NoError(t, utils.EnsureDirs(dirs.MultiPackageDir, dirs.DamlPackageDir))
 
-		// create multi-package.yaml
-		multiPackage := multipackage.MultiPackage{
-			Packages: []string{"./daml-package"},
-		}
+		if !tc.PackageOnly {
+			dirs.MultiPackageDir = filepath.Join(tmpDir, "multi-package")
+			dirs.DamlPackageDir = filepath.Join(dirs.MultiPackageDir, "daml-package")
 
-		for compName, version := range tc.MultiPackageComponents {
-			// TODO DeprecatedOverrideComponents is being used here because
-			// the Components field is being ignored (`yaml:"-"`) in the YAML marshaling
-			sv, err := semver.StrictNewVersion(version)
-			require.NoError(t, err)
+			require.NoError(t, utils.EnsureDirs(dirs.MultiPackageDir, dirs.DamlPackageDir))
 
-			multiPackage.DeprecatedOverrideComponents = map[string]*sdkmanifest.Component{
-				compName: {
+			// create multi-package.yaml
+			multiPackage := multipackage.MultiPackage{
+				Packages:                     []string{"./daml-package"},
+				DeprecatedOverrideComponents: make(map[string]*sdkmanifest.Component),
+			}
+
+			for compName, version := range tc.MultiPackageComponents {
+				// TODO DeprecatedOverrideComponents is being used here because
+				// the Components field is being ignored (`yaml:"-"`) in the YAML marshaling
+				sv, err := semver.StrictNewVersion(version)
+				require.NoError(t, err)
+				multiPackageComponent := sdkmanifest.Component{
 					Name:    compName,
 					Version: sdkmanifest.AssemblySemVer(sv),
-				},
-			}
-		}
-		require.NoError(t,
-			os.WriteFile(filepath.Join(dirs.MultiPackageDir, "multi-package.yaml"), testutil.MustMarshal(t, multiPackage), 0666),
-		)
+				}
 
+				multiPackage.DeprecatedOverrideComponents[compName] = &multiPackageComponent
+			}
+			require.NoError(t,
+				os.WriteFile(filepath.Join(dirs.MultiPackageDir, "multi-package.yaml"), testutil.MustMarshal(t, multiPackage), 0666),
+			)
+		} else {
+			dirs.DamlPackageDir = filepath.Join(tmpDir, "daml-package")
+			require.NoError(t, utils.EnsureDirs(dirs.DamlPackageDir))
+		}
 		// create daml.yaml
-		damlPackage := damlpackage.DamlPackage{}
+		damlPackage := damlpackage.DamlPackage{
+			DeprecatedOverrideComponents: make(map[string]*sdkmanifest.Component),
+		}
 		for compName, version := range tc.PackageComponents {
 			sv, err := semver.StrictNewVersion(version)
 			require.NoError(t, err)
-			damlPackage.DeprecatedOverrideComponents = map[string]*sdkmanifest.Component{
-				compName: {
-					Name:    compName,
-					Version: sdkmanifest.AssemblySemVer(sv),
-				},
+			packageComponent := sdkmanifest.Component{
+				Name:    compName,
+				Version: sdkmanifest.AssemblySemVer(sv),
 			}
+			damlPackage.DeprecatedOverrideComponents[compName] = &packageComponent
 		}
 		require.NoError(t,
 			os.WriteFile(filepath.Join(dirs.DamlPackageDir, "daml.yaml"), testutil.MustMarshal(t, damlPackage), 0666),
@@ -118,7 +220,33 @@ func (suite *MainSuite) TestComponentOverlay() {
 	for _, tc := range componentOverlayTestCases {
 		t.Run(tc.Name, func(t *testing.T) {
 			setupTestCase(tc)
-			// TODO test the --help command and the resolution
+			t.Run(tc.Name+" dpm --help", func(t *testing.T) {
+
+				output := runHelpCommand(t)
+
+				// assert number of cmds in output as expected
+				_, trimmedOutput, _ := strings.Cut(output, "Dpm-SDK Commands\n")
+				trimmedOutput, _, _ = strings.Cut(trimmedOutput, "\nAdditional Commands")
+				count := strings.Count(trimmedOutput, "\n")
+				assert.Equal(t, len(tc.ExpectedHelpCommands), count)
+
+				// assert values in output as expected
+				for _, command := range tc.ExpectedHelpCommands {
+					assert.Contains(t, trimmedOutput, command)
+				}
+			})
+			t.Run(tc.Name+" dpm resolve", func(t *testing.T) {
+				deepResolution := runResolveCommand(t)
+
+				assert.Len(t, lo.Values(deepResolution.Packages)[0].ComponentsV2, len(tc.ExpectedResolutionComponents))
+
+				for component, version := range tc.ExpectedResolutionComponents {
+					assert.Contains(t, lo.Values(deepResolution.Packages)[0].ComponentsV2, component)
+					resolvedVersion := lo.Values(deepResolution.Packages)[0].ComponentsV2[component]["version"]
+					assert.Equal(t, version, resolvedVersion)
+				}
+
+			})
 		})
 	}
 
