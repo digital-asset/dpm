9c1d5d91801ca485964ae432de3c90c7b224b396
georgeorellanaDA <george.orellana@digitalasset.com>
2026-04-30 11:30:10 -0400

    Implement resolve and help, need to implement multiless

--- cmd/dpm/cmd/component_overlay_test.go
+++ cmd/dpm/cmd/component_overlay_test.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"daml.com/x/assistant/pkg/assistantconfig"
@@ -12,6 +13,8 @@ import (
 	"daml.com/x/assistant/pkg/testutil"
 	"daml.com/x/assistant/pkg/utils"
 	"github.com/Masterminds/semver/v3"
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -25,7 +28,6 @@ type ComponentOverlayTestCase struct {
 }
 
 var componentOverlayTestCases = []ComponentOverlayTestCase{
-	// TODO fill out add the remaining test cases
 	{
 		Name:                         "1",
 		WorkingDir:                   MultiPackageWorkingDir,
@@ -33,6 +35,96 @@ var componentOverlayTestCases = []ComponentOverlayTestCase{
 		PackageComponents:            nil,
 		ExpectedHelpCommands:         []string{"foo"},
 		ExpectedResolutionComponents: map[string]string{"foo": "0.0.1"},
+	},
+	{
+		Name:                         "2",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1"},
+		PackageComponents:            nil,
+		ExpectedHelpCommands:         []string{"foo"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.1"},
+	},
+	{
+		Name:                         "3",
+		WorkingDir:                   MultiPackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1"},
+		PackageComponents:            map[string]string{"foo": "0.0.2"},
+		ExpectedHelpCommands:         []string{"foo"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.1"},
+	},
+	{
+		Name:                         "4",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1"},
+		PackageComponents:            map[string]string{"foo": "0.0.2"},
+		ExpectedHelpCommands:         []string{"foo"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.2"},
+	},
+	{
+		Name:                         "5",
+		WorkingDir:                   MultiPackageWorkingDir,
+		MultiPackageComponents:       nil,
+		PackageComponents:            map[string]string{"foo": "0.0.2"},
+		ExpectedHelpCommands:         []string{},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.2"},
+	},
+	{
+		Name:                         "6",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       nil,
+		PackageComponents:            map[string]string{"foo": "0.0.2"},
+		ExpectedHelpCommands:         []string{"foo"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.2"},
+	},
+	{
+		Name:                         "7",
+		WorkingDir:                   MultiPackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1", "meep": "0.0.1"},
+		PackageComponents:            nil,
+		ExpectedHelpCommands:         []string{"foo", "meep"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.1", "meep": "0.0.1"},
+	},
+	{
+		Name:                         "8",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1", "meep": "0.0.1"},
+		PackageComponents:            nil,
+		ExpectedHelpCommands:         []string{"foo", "meep"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.1", "meep": "0.0.1"},
+	},
+	{
+		Name:                         "9",
+		WorkingDir:                   MultiPackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1"},
+		PackageComponents:            map[string]string{"meep": "0.0.2"},
+		ExpectedHelpCommands:         []string{"foo"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.1", "meep": "0.0.2"},
+	},
+	{
+		Name:                         "10",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       map[string]string{"foo": "0.0.1"},
+		PackageComponents:            map[string]string{"meep": "0.0.2"},
+		ExpectedHelpCommands:         []string{"foo", "meep"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.1", "meep": "0.0.2"},
+	},
+	{
+		//no multi-pkg structure - in pkg dir
+		Name:                         "11",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       nil,
+		PackageComponents:            map[string]string{"foo": "0.0.2"},
+		ExpectedHelpCommands:         []string{"foo"},
+		ExpectedResolutionComponents: map[string]string{"foo": "0.0.2"},
+	},
+	{
+		//no multi-pkg structure - in pkg dir
+		Name:                         "12",
+		WorkingDir:                   PackageWorkingDir,
+		MultiPackageComponents:       nil,
+		PackageComponents:            nil,
+		ExpectedHelpCommands:         []string{}, //blank DS or nil
+		ExpectedResolutionComponents: map[string]string{},
 	},
 }
 
@@ -118,7 +210,33 @@ func (suite *MainSuite) TestComponentOverlay() {
 	for _, tc := range componentOverlayTestCases {
 		t.Run(tc.Name, func(t *testing.T) {
 			setupTestCase(tc)
-			// TODO test the --help command and the resolution
+			t.Run(tc.Name+" dpm --help", func(t *testing.T) {
+
+				output := runHelpCommand(t)
+
+				// assert number of cmds in output as expected
+				_, trimmedOutput, _ := strings.Cut(output, "Dpm-SDK Commands\n")
+				trimmedOutput, _, _ = strings.Cut(trimmedOutput, "\nAdditional Commands")
+				count := strings.Count(trimmedOutput, "\n")
+				assert.Equal(t, len(tc.ExpectedHelpCommands), count)
+
+				// assert values in output as expected
+				for _, command := range tc.ExpectedHelpCommands {
+					assert.Contains(t, trimmedOutput, command)
+				}
+			})
+			t.Run(tc.Name+" dpm resolve", func(t *testing.T) {
+				deepResolution := runResolveCommand(t)
+
+				assert.Len(t, lo.Values(deepResolution.Packages)[0].ComponentsV2, len(tc.ExpectedResolutionComponents))
+
+				for component, version := range tc.ExpectedResolutionComponents {
+					assert.Contains(t, lo.Values(deepResolution.Packages)[0].ComponentsV2, component)
+					resolvedVersion := lo.Values(deepResolution.Packages)[0].ComponentsV2[component]["version"]
+					assert.Equal(t, version, resolvedVersion)
+				}
+
+			})
 		})
 	}
 
