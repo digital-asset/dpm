f30de49a91b120b7d299274a247f118de61ea56e
Sammy Abed <sammy.abed@digitalasset.com>
2026-06-11 22:36:04 -0400

    meep

--- cmd/dpm/cmd/add/component/component.go
+++ cmd/dpm/cmd/add/component/component.go
@@ -2,6 +2,7 @@ package component
 
 import (
 	"os"
+	"slices"
 	"strings"
 
 	"daml.com/x/assistant/pkg/assistantconfig"
@@ -9,6 +10,7 @@ import (
 	"github.com/goccy/go-yaml"
 	"github.com/goccy/go-yaml/ast"
 	"github.com/goccy/go-yaml/parser"
+	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 	"oras.land/oras-go/v2/registry"
 )
@@ -53,35 +55,35 @@ func addToPackage(path, component string) error {
 		return err
 	}
 
-	out := insertAfterLastComponent(b, strings.TrimRight(string(item), "\n"))
+	out := appendToYaml(b, strings.TrimRight(string(item), "\n"))
 	return os.WriteFile(path, out, 0644)
 }
 
-func insertAfterLastComponent(b []byte, item string) []byte {
-	file, _ := parser.ParseBytes(b, 0)
+func appendToYaml(b []byte, item string) []byte {
+	item = "  " + item + "\n"
 
-	components := file.Docs[0].
-		Body.(*ast.MappingNode).
-		Values[1].
-		Value.(*ast.SequenceNode)
+	file, _ := parser.ParseBytes(b, 0)
+	components := findField(file.Docs[0], "components")
+	lines := strings.SplitAfter(string(b), "\n")
+
+	if components == nil {
+		return []byte(strings.Join(append(lines, "components:\n", item), ""))
+	}
 
 	insertAt := components.GetToken().Position.Line
-	indent := strings.Repeat(" ", components.GetToken().Position.Column)
-
 	if len(components.Entries) > 0 {
 		last := components.Entries[len(components.Entries)-1]
 		insertAt = last.GetToken().Position.Line
-
-		line := strings.SplitAfter(string(b), "\n")[insertAt-1]
-		indent = line[:len(line)-len(strings.TrimLeft(line, " "))]
 	}
 
-	lines := strings.SplitAfter(string(b), "\n")
-
-	lines = append(
-		lines[:insertAt],
-		append([]string{indent + item + "\n"}, lines[insertAt:]...)...,
-	)
-
+	lines = slices.Insert(lines, insertAt, item)
 	return []byte(strings.Join(lines, ""))
+}
+
+func findField(doc *ast.DocumentNode, field string) *ast.SequenceNode {
+	body := doc.Body.(*ast.MappingNode)
+	r, _ := lo.Find(body.Values, func(n *ast.MappingValueNode) bool {
+		return n.Key.String() == field
+	})
+	return r.Value.(*ast.SequenceNode)
 }
