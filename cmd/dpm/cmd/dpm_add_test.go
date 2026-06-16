47c988948daac0d2b829792b5eacf956efedc053
Sammy Abed <sammy.abed@digitalasset.com>
2026-06-16 10:59:43 -0400

    feedback

--- cmd/dpm/cmd/dpm_add_test.go
+++ cmd/dpm/cmd/dpm_add_test.go
@@ -15,22 +15,7 @@ func (suite *MainSuite) TestDpmAddComponentCommand() {
 
 	newComponent := "oci://example.com/newly-added:4.5.6"
 
-	t.Run("modifies the daml.yaml file", func(t *testing.T) {
-		projectDir := testutil.ActivateDamlYamlForTest(t, `
-components:
-  - damlc:1.2.3
-  - oci://example.com/some/component:1.2.3
-`)
-
-		cmd := createStdTestRootCmd(t, "add", "component", newComponent)
-		require.NoError(t, cmd.Execute())
-
-		damlYaml, err := os.ReadFile(filepath.Join(projectDir, "daml.yaml"))
-		require.NoError(t, err)
-		assert.Contains(t, string(damlYaml), "- "+newComponent)
-	})
-
-	t.Run("modifies the daml.yaml file", func(t *testing.T) {
+	t.Run("add new component in single-package project", func(t *testing.T) {
 		projectDir := testutil.ActivateDamlYamlForTest(t, `
 components:
   - damlc:1.2.3
@@ -45,7 +30,7 @@ components:
 		assert.Contains(t, string(newContent), "- "+newComponent)
 	})
 
-	t.Run("modifies the daml.yaml file", func(t *testing.T) {
+	t.Run("add new component in  multi-package project", func(t *testing.T) {
 		testutil.ActivateMultiPackageYamlForTest(t, `
 components:
   - damlc:1.2.3
